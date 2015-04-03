c5e58e944e6661461dba6aa47b8d682f73b8311d
Fedor Kulikov <f.kulikov@gmail.com>
2015-04-03 09:47:49 +0200

    solved 2.61 and 2.62

---
+++ 2_61.scm
@@ -0,0 +1,5 @@
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< x (car set)) (cons x set))
+        ((= x (car set)) set)
+        (else (cons (car set) (adjoin-set x (cdr set))))))
---
+++ 2_62.scm
@@ -0,0 +1,7 @@
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (let ((x1 (car set1)) (x2 (car set2)) (rest1 (cdr set1)) (rest2 (cdr set2)))
+                (cond ((= x1 x2) (cons x1 (union-set rest1 rest2)))
+                      ((< x1 x2) (cons x1 (union-set rest1 set2)))
+                      ((> x1 x2) (cons x2 (union-set set1 rest2))))))))
