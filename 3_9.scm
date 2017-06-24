cdcd97a54c5d315cee5b2d429b040be130b4f55c
Fedor Kulikov <f.kulikov@gmail.com>
2017-06-25 00:42:06 +0500

    Fixed solution of 3.9 and added one for 3.10

--- 3_9.scm
+++ 3_9.scm
@@ -1,20 +1,22 @@
 (define (factorial n)
   (if (= n 1) 1 (* n (factorial (- n 1)))))
 
-;; Evaluating given function with argument equals 6 will
-;; lead to creation of the following set of environments:
 ;;
-;; E1: n=6 (if (= n 1) ... )
-;; E2: n=6 (* n (factorial (- n 1)))
-;; E3: n=5 (if (= n 1) ... )
-;; E4: n=5 (* n (factorial (- n 1)))
-;; E5: n=4 (if (= n 1) ... )
-;; E6: n=4 (* n (factorial (- n 1)))
-;; E7: n=3 (if (= n 1) ... )
-;; E8: n=3 (* n (factorial (- n 1)))
-;; E9: n=2 (if (= n 1) ...
-;; E10: n=2 (* n (factorial (- n 1)))
-;; E11: n=1 (if (= n 1) ...
+;;
+;;               +------------------------------------------------------+
+;;               |                                                      |
+;;               +----^-------------------^------------------------^----+
+;; (factorial 6)      |                   |                        |
+;;               +---------+         +---------+              +---------+
+;;               |n: 6     |         |n: 5     |              |n: 1     |
+;;         E1+--->         |   E2+--->         |  ...   E6+--->         |
+;;               |         |         |         |              |         |
+;;               +---------+         +---------+              +---------+
+;;
+;;              (if (= n 1)         (if (= n 1)              (if (= n 1)
+;;                  1                   1                        1
+;;                  (* n ... ))         (* n ... ))              (* n ... ))
+;;
 
 (define (factorial n) (fact-iter 1 1 n))
 (define (fact-iter product counter max-count)
@@ -24,13 +26,33 @@
                  (+ counter 1)
                  max-count)))
 
-;; Evaluating this version of factorial function with argument 6 will lead to:
 ;;
-;; E1: n=6 (fact-iter 1 1 n)
-;; E2: product=1 counter=1 max-count=6 (if (> counter max-count) ... )
-;; E3: product=1 counter=2 max-count=6 (if (> counter max-count) ... )
-;; E4: product=2 counter=3 max-count=6 (if (> counter max-count) ... )
-;; E5: product=6 counter=4 max-count=6 (if (> counter max-count) ... )
-;; E6: product=24 counter=5 max-count=6 (if (> counter max-count) ... )
-;; E7: product=120 counter=6 max-count=6 (if (> counter max-count) ... )
-;; E8: product=720 counter=7 max-count=6 (if (> counter max-count) ... )
+;;               +-----------------------------------------------------
+;;               |                                                      ...
+;;               +----^-------------------^-----------------------^----
+;; (factorial 6)      |                   |                       |
+;;               +---------+         +------------+         +------------+
+;;               |n: 6     |         |product: 1  |         |product: 1  |
+;;         E1+--->         |   E2+--->counter: 1  |   E3+--->counter: 2  |
+;;               |         |         |max-count: 6|         |max-count: 6|
+;;               +---------+         +------------+         +------------+
+;;
+;;            (fact-iter 1 1 n)      (if (> counter         (if (> counter
+;;                                          max-count)             max-count)
+;;                                     ...                    ...
+;;                                   )                      )
+;;
+;;                     -------------------------------------------+
+;;             ...                                                |
+;;                     ------------^-----------------------^------+
+;;                                 |                       |
+;;                            +------------+         +------------+
+;;                            |product: 120|         |product: 720|
+;;                      E6+--->counter: 6  |   E7+--->counter: 7  |
+;;                            |max-count: 6|         |max-count: 6|
+;;                            +------------+         +------------+
+;;
+;;                            (if (> counter         (if (> counter
+;;                                   max-count)             max-count)
+;;                              ...                    ...
+;;                            )                      )
